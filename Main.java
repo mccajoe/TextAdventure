6c3a045808e702224d8a88ad7f463fec385b01e4
mccajoe <joeymccauley@rogers.com>
2014-12-08 14:36:25 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,32 @@
+//package game;
+
+import java.util.*;
+
+public class TextGam {
+	public static void main(String[] args){
+		Map map = new Map();
+		String currentsd="";
+		String currentld="";
+		int currentLoc=5;
+		char command='a';
+		
+		
+		Scanner keyb = new Scanner(System.in);
+		
+		System.out.println("You are in the " + currentsd + ". "+currentld);
+		
+		command= keyb.next();
+		switch(command){
+			case 'w': if(){
+				
+					}
+			case 's':
+			case 'n':
+			case 'e':
+			default: 
+		}
+		
+		
+	}
+	
+}
